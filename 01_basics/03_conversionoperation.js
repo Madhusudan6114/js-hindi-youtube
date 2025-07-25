43a9223302132efaaecb26026d517ad450454694
Madhusudan6114 <167520567+Madhusudan6114@users.noreply.github.com>
2025-07-25 13:44:06 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,32 @@
+// let score="33"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let valueInNumber=Number(score)
+
+
+//let score=true   //  value inmumber will contain  1
+//let score=NULL   //                              zer0
+//let score =33abc // value will be NaN
+
+// console.log(typeof valueInNumber )
+
+let loggedin=1;
+let booleanloggedin=Boolean(loggedin)
+//console.log(booleanloggedin);//returning true
+
+// 1=>true // o=>false
+//""=>false //" word"=>true
+
+let somenumber=33
+
+let stringnumber=String(33)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
+
+
+
+
+
