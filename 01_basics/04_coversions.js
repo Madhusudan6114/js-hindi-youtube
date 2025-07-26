1734113e2c629d1659ed1ffcf37778a2bff2336e
Madhusudan6114 <167520567+Madhusudan6114@users.noreply.github.com>
2025-07-26 08:09:37 +0000

    datarypesummary

---
+++ 01_basics/04_coversions.js
@@ -0,0 +1,15 @@
+//console.log(2>1)
+
+console.log("2">1)
+console.log("02">1)//avoid hese types of conversions
+
+
+// console.log(null>0) =>false
+// console.log(null==0 )=>false
+// console.log(null>=0)=>true
+
+// console.log(undefined==0);  //avoid theses type of conversions
+// console.log(undefined>0)
+
+//=== //strict check it checks datatypes also whether they are equal are not
+console
---
+++ 01_basics/datatypesummary.js
@@ -0,0 +1,13 @@
+// primitive
+
+// 7 types :string,number,Bo0lean,null,undefined,Symbol,BigInt 
+
+//Reference type (non primitive)
+//array,Objects,functions;
+
+// const id=Symbol('123')
+// const anotherid=Symbol('123')
+// console.log(id===anotherid)
+//output will false
+
+const bigNumber=123456789n //=>bigInt
