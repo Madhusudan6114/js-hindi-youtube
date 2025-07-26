5e7aab7587e7abeb9f1d03cbfe253828550b287f
Madhusudan6114 <167520567+Madhusudan6114@users.noreply.github.com>
2025-07-26 14:19:12 +0000

    strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,31 @@
+const name="Madhusudan"
+const repoCount=50
+//console.log(name+repoCount+"Value");
+
+//use
+// console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`)
+
+
+
+// const gamename=new String(`abc`)
+
+// console.log(gamename[0])
+// console.log(gamename._proto_)
+
+// console.log(gamename.length)//i csn use hti sfunnction because her string is created as object
+// console.log(gamename.toUppercase())
+// console.log(gamename)
+
+console.log(gamename.charAt(2))
+console.log(gamename.indexOf('t'))
+
+const newString=gamename.substring(0,4)
+console.log(newString)
+const anotherstring=gamename.slice(-8,4)
+console.log(anotherstring)
+
+
+
+
+
+
